a0d701483c6b5febc6af4980f0d68cf224686727
Rajan Garhwal <51743092+rajangarhwal@users.noreply.github.com>
2020-08-13 02:29:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,144 @@
 var express = require("express"),
     app = express(),
     mongoose = require('mongoose'),
-    bodyparser = require("body-parser");
+    passport = require("passport"),
+    bodyparser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportlocalMongoose = require("passport-local-mongoose"),
+    request = require("request"),
+    User = require("./models/user");
 
 
+
+
+mongoose.connect('mongodb://localhost/App_Wings', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 
-mongoose.connect('mongodb://localhost/App_Wings', { useNewUrlParser: true, useUnifiedTopology: true });
-var customerSchema = new mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    mail: String,
-    password: String,
-    contact: Number
+app.use(require("express-session")({
+    secret: "Something is usual but in secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
 });
 
-var Customer = mongoose.model('Customer', customerSchema);
 
 
 app.get("/", function(req, res) {
-    res.render("Home");
+
+    res.render("Home", { currentUser: req.user });
 })
 
-app.get("/Login/", function(req, res) {
-    res.render("Login");
+
+
+
+
+app.get("/movie_rating/", isLoggedIn, function(req, res) {
+    res.render("movie_rating");
 })
-app.get("/Sign_Up/", function(req, res) {
+
+app.post("/movie_rating/results/", isLoggedIn, function(req, res) {
+
+    var query = req.body.mvname;
+    console.log(query);
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+    console.log(url);
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var mv = JSON.parse(body);
+            console.log(mv["Search"]);
+            res.render("movie_rating_results", { mvd: mv });
+        }
+
+    });
+})
+
+
+
+
+
+
+
+// Register form
+
+app.get("/Sign_Up", function(req, res) {
     res.render("Sign_Up");
 })
 
-app.post("/add", function(req, res) {
+// handle Sign Up logic
+
+app.post("/Sign_Up", function(req, res) {
     var newfirst = req.body.newfirst;
     var newlast = req.body.newlast;
     var newemail = req.body.newemail;
-    var newpass1 = req.body.password;
+    var password = req.body.password;
     var newcontact = req.body.newcontact;
+    var username = req.body.username;
 
-    Customer.create({
+    var newUser = new User({
+        username: username,
         first_name: newfirst,
         last_name: newlast,
         mail: newemail,
-        password: newpass1,
         contact: newcontact
-    }, function(err, customer) {
-        if (err) {
-            console.log("Got Error During Addition of Database")
-            console.log(err);
-        } else {
-            console.log("NEw Customer Data is Added");
-            console.log(customer);
-        }
     });
-    res.redirect("/");
+
+    User.register(newUser, password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("Sign_Up");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/");
+        });
+    });
 })
-app.listen(process.env.PORT, process.env.IP, function(req, res) {
-    console.log("Server Started !!");
+
+
+// show login form 
+app.get("/Login", function(req, res) {
+    res.render("Login");
+})
+
+// handling login logic
+
+app.post("/Login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/Login"
+}), function(req, res) {});
+
+
+// logout route 
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+// login check
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/Login")
+}
+
+app.listen(3099, function(req, res) {
+    console.log("Server Started !!! ");
 })
